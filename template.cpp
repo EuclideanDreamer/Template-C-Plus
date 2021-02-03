7771314048386eccd7d61120dfd33133d57cc91a
EuclideanDreamer <78236576+EuclideanDreamer@users.noreply.github.com>
2021-02-02 16:02:48 -0800

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,36 @@
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+//* Program name	: 	*
+//*	* 
+//* Written by	: Your Name 	*
+//*	* 
+//* Purpose	: Maximum 3 sentence desciption of what this	* 
+//*	  c++ program is suppose to do.	* 
+//*	* 
+//*---------------------------------------------------------------------* 
+//* Change Log:	* 
+//*	Revision	* 
+//*	Date	Changed   Rel Ver Mod Purpose	*
+//* 01/29/21 Lastname  001.000.000 Initial release of program	*
+//*	* 
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+#include <iostream>
+#include <string>
+//include
+using namespace std;
+//using
+//constants
+//variables
+//main
+int main()
+{
+    
+    
+    cout << "Program Data and Basic instructions" << endl;
+    //first executable
+    //Algorithm 
+    char q;
+    cout << "\nPress any key to Exit.";
+    cin.ignore(2, '\n');
+    cin.get(q);
+    return 0;
+}
